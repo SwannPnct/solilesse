e01f6bbed066baeadf98bac213e48db869824d47
Swann Pincet <swann.pincet@gmail.com>
2024-03-09 15:28:12 +0100

    init and base list

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const {data: announcements, pending, error} = await useFetch('/api/announcements', {
+        lazy: true
+    })
+</script>
+
+<template>
+    <div v-if="pending">loading...</div>
+    <div v-else-if="!!error">
+        <strong class="text-red-500">Erreur.</strong>
+    </div>
+    <ul v-else>
+        <li v-for="announcement in announcements.results">
+            {{ announcement.id }}
+        </li>
+    </ul>
+</template>
---
+++ server/api/announcements.get.js
@@ -0,0 +1,25 @@
+export default defineEventHandler(async (event) => {
+  const baseURL =
+    "https://bodacc-datadila.opendatasoft.com/api/explore/v2.1/catalog/datasets/annonces-commerciales/records?limit=20";
+
+  let data;
+  let json;
+
+  try {
+    data = await fetch(baseURL);
+    json = await data.json();
+  } catch {
+    data = {
+      status: 500,
+    };
+  }
+
+  if ([400, 500].includes(data.status)) {
+    throw createError({
+      statusCode: data.status,
+      statusMessage: json.message ?? "Erreur inconnue.",
+    });
+  } else {
+    return json;
+  }
+});
