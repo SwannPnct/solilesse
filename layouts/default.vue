6807f0508a302c8b5661e3684882e30013dd469d
Swann Pincet <swann.pincet@gmail.com>
2024-03-19 16:31:53 +0100

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-     <div class="container py-12 relative">
-        <h1 class="font-semibold text-xl mb-16">Solilesse</h1>
-        <slot />
+     <div class="container relative">
+         <div class="flex py-6">
+            <a class="block flex gap-4 items-center cursor-pointer" href="/">
+               <img class="size-10" src="~/assets/images/logo.webp" alt="Solilesse logo"/>
+               <h1 class="font-semibold text-xl">
+                  Solilesse
+               </h1>
+            </a>
+         </div>
+         <div class="container py-12">
+            <slot />
+         </div>
      </div>
 </template>
