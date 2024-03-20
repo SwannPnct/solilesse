e6ea8e040f434000424afa76f8a6aa0bdc366c3c
Swann Pincet <swann.pincet@gmail.com>
2024-03-20 11:20:40 +0100

    annonces route

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
      <div class="container relative">
-         <div class="flex py-6">
+         <div class="flex justify-between py-6">
             <NuxtLink class="block flex gap-4 items-center cursor-pointer" to="/">
                <img class="size-10" src="~/assets/images/logo.webp" alt="Solilesse logo"/>
                <h1 class="font-semibold text-xl">
                   Solilesse
                </h1>
             </NuxtLink>
+            <div class="flex gap-4 items-center">
+               <NuxtLink class="block hover:underline" to="/annonces">
+                  Annonces
+               </NuxtLink>
+            </div>
          </div>
          <div class="container py-12">
             <slot />
