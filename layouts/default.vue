5314f1040421733910b908c294617948f77a118a
Swann Pincet <swann.pincet@gmail.com>
2024-03-20 11:54:10 +0100

    header ref

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,22 @@
+<script setup>
+   const links = [
+      ["/annonces", "Annonces"],
+      ["/supprimees", "Supprimées"]
+   ]
+</script>
+
 <template>
      <div class="container relative">
-         <div class="flex justify-between py-6">
+         <div class="flex py-6 items-center">
             <NuxtLink class="block flex gap-4 items-center cursor-pointer" to="/">
                <img class="size-10" src="~/assets/images/logo.webp" alt="Solilesse logo"/>
                <h1 class="font-semibold text-xl">
                   Solilesse
                </h1>
             </NuxtLink>
-            <div class="flex gap-4 items-center">
-               <NuxtLink class="block hover:underline" to="/annonces">
-                  Annonces
+            <div class="flex gap-4 items-center m-auto">
+               <NuxtLink v-for="link in links" exactActiveClass="underline" class="block hover:underline" :to="link[0]">
+                  {{ link[1] }}
                </NuxtLink>
             </div>
          </div>
